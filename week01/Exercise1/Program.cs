bf674e58b986fced283a943aebefccd76b990f9d
Rsilva2004 <rafaelbasilva1723@gmail.com>
2026-07-01 19:02:25 -0300

    Exercise 1 done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,7 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Im still stressing out about this assignment");
+        Console.WriteLine("What is your first name?");
+        string firstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastName = Console.ReadLine();
+        
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
