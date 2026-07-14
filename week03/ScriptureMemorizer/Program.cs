cc87a41a8fa174e64951ce2c445b30c159090d33
Rsilva2004 <rafaelbasilva1723@gmail.com>
2026-07-14 19:30:28 -0300

    Updating scriptures at program.cs

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -5,8 +5,8 @@ using System;
 
   List<Scripture> scriptures = new List<Scripture>
   {
-      new Scripture(new Reference("John", 3, 16), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life."),
-      new Scripture(new Reference("Proverbs", 3, 5), "Trust in the Lord with all your heart and lean not on your own understanding;"),
+      new Scripture(new Reference("John", 3, 16, 17), "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life. For God sent not his Son into the world to condemn the world; but that the world through him might be saved."),
+      new Scripture(new Reference("Proverbs", 3, 5, 6), "Trust in the Lord with all your heart and lean not on your own understanding; in all your ways submit to him, and he will make your paths straight."),
       new Scripture(new Reference("1 Nephi", 3, 7), "I will go and do the things which the Lord hath commanded, for I know that the Lord giveth no commandments unto the children of men, save he shall prepare a way for them that they may accomplish the thing which he commandeth them."),
       new Scripture(new Reference("D&C", 6, 36), "Look unto me in every thought; doubt not, fear not.")
   };
--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -7,15 +7,22 @@ public class Reference
     private int _verse;
     private int _endVerse;
 
-    public Reference(string book, int chapter, int verse)
+    public Reference(string book, int chapter, int verse, int endVerse = 0)
     {
         _book = book;
         _chapter = chapter;
         _verse = verse;
+        _endVerse = endVerse;
     }
 
     public string GetReferenceText()
     {
+
+          if (_endVerse > 0)
+    {
+        return $"{_book} {_chapter}:{_verse}-{_endVerse}";
+    }
+
         return $"{_book} {_chapter}:{_verse}";
     }
 }
